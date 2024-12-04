8c823e4d5ac541c75ab6f6a45b149fac09383001
makarbench <48049434+makarbench@users.noreply.github.com>
2024-12-04 11:19:44 +0400

    Update token_creation_2.py

--- token_creation_2.py
+++ token_creation_2.py
@@ -4,26 +4,26 @@ import secrets
 import hashlib
 
 
-def generate_access_token_2(long=10):
+def generate_access_token_2(lng=10):
     """
     crypto token creation by using secrets.
     """
     characters = string.ascii_letters + string.digits + string.punctuation
-    token = ''.join(secrets.choice(characters) for _ in range(long))
+    token = ''.join(secrets.choice(characters) for _ in range(lng))
     return token
 
 
 
-def generate_access_token_3(long=32, salt_long=10):
+def generate_access_token_3(lng=32, salt_lng=10):
     """
     crypto token creation by using secrets + salt 
     using secrets and hashlib vs cryptoprediction attack.
     """
 
     # salt generated
-    salt = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(salt_long))
+    salt = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(salt_lng))
     # token generated
-    raw_token = ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(long))
+    raw_token = ''.join(secrets.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(lng))
 
     # salt + token
     combined = (salt + raw_token).encode('utf-8')
